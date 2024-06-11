c246e6ddb85ebd7f4202c22d742b15da5dd78b61
sq1d0224 <isho05010224@gmail.com>
2024-06-11 13:02:35 +0000

    管理者機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
